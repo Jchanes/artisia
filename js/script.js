8f0114240203cd84d2074b234eac72433a85f77e
chanito <qwertyuiop.xd1@gmail.com>
2026-03-30 19:08:59 -0600

    On branch testing  Changes to be committed: 	new file:   assets/img/31003650_7756902.svg 	new file:   assets/img/background.svg 	modified:   css/comunidades.css 	new file:   css/global.css 	new file:   css/global.css~ 	modified:   css/style.css 	modified:   global.html 	modified:   js/script.js 	modified:   recursos.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const btn = document.getElementById('verMas');
 const extra = document.getElementById('coleccionesExtra');
+const mapaGlobal = document.getElementById('mapa-global');
+const culturaCards = document.querySelectorAll('.cultura-card');
+
 
 if (btn && extra) {
   btn.addEventListener('click', function () {
@@ -7,5 +10,20 @@ if (btn && extra) {
     btn.textContent = extra.classList.contains('visible')
       ? 'Ver menos ↑'
       : 'Ver más ↓';
+  });
+}
+
+if (mapaGlobal && culturaCards.length > 0) {
+  culturaCards.forEach(card => {
+    card.addEventListener('click', () => {
+
+      const lugar = card.dataset.lugar;
+      mapaGlobal.src =
+        `https://maps.google.com/maps?q=${encodeURIComponent(lugar)}&output=embed`;
+
+      culturaCards.forEach(c => c.classList.remove('activa'));
+      card.classList.add('activa');
+
+    });
   });
 }
